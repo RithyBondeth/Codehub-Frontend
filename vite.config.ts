f61a5024a007500d94b922b0da670219f5f1081e
RithyB <ctrcomputer@Bondeths-Macbook-Pro-2.local>
2024-11-01 19:18:12 +0700

    fix route deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from "tailwindcss"
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  server: {
+    middlewareMode: true,
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
