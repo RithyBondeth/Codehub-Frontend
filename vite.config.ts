5f7f6155726a4bbabcdc353d95d141569a8da293
RithyB <ctrcomputer@Bondeths-Macbook-Pro-2.local>
2024-11-01 19:31:12 +0700

    done

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import tailwindcss from "tailwindcss"
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  server: {
-    middlewareMode: true,
-  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
