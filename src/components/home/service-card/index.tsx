90475214b6e62f99a4f9ca3636581b06a54a143c
RithyB <ctrcomputer@Bondeths-Macbook-Pro-2.local>
2024-11-01 11:18:37 +0700

    fix animation

--- src/components/home/service-card/index.tsx
+++ src/components/home/service-card/index.tsx
@@ -1,24 +1,16 @@
 import { useTranslation } from "react-i18next"
 import Divider from "../../utilities/styles/divider"
-import gsap from "gsap"
 import { useEffect } from "react"
 import { useServiceStore } from "../../../stores/api/service/service.store"
 import { ALL_SERVICE_URL } from "../../../constants/api/service.api"
 import { useLanguageStore } from "../../../stores/language/language.store"
 import ServiceSkeleton from "./service-skeleton"
-import { useDrawerStore } from "../../../stores/drawer/drawer.store"
 
 
 export default function ServiceCard() { 
     const { t } = useTranslation()    
-    const isOpen = useDrawerStore((state) => state.isOpen)
     const { language } = useLanguageStore()
     const { data, loading, error, fetchData } = useServiceStore()
-
-   useEffect(() => {
-    gsap.from("#service-box", { duration: 1,x: 500, opacity: 0, ease: "power1.out"})
-    gsap.to("#service-box", { duration: 1, x: 0, opacity: 1, ease: "power1.out" })
-   })
 
    useEffect(() => {
         const controller = new AbortController()
@@ -51,12 +43,12 @@ export default function ServiceCard() {
     return (
         <div>
             {data && <div className="w-full flex flex-col items-center gap-5">
-                <p id={`${!isOpen && "service-box"}`} className="text-3xl text-center font-medium tablet-md:text-2xl phone-xl:text-xl">{t("pages.home.home-service.label")}</p>
-                <Divider id={`${!isOpen && "service-box"}`}/>
-                <p id={`${!isOpen && "service-box"}`} className="leading-10">{language === "kh" ? data[0].khmerContent : data[0].content}</p>
+                <p className="text-3xl text-center font-medium tablet-md:text-2xl phone-xl:text-xl">{t("pages.home.home-service.label")}</p>
+                <Divider/>
+                <p className="leading-10">{language === "kh" ? data[0].khmerContent : data[0].content}</p>
                 <div className="grid grid-cols-2 gap-5 phone-xl:grid-cols-1">
                     {data.map((service) => (
-                        <div id={`${!isOpen && "service-box"}`} className="shadow-md mt-3 px-3 py-4 flex items-start gap-3 cursor-pointer shadow-normal duration-300 hover:shadow-3d-primary tablet-lg:flex-col phone-xl:items-center" key={service.id}>
+                        <div className="shadow-md mt-3 px-3 py-4 flex items-start gap-3 cursor-pointer shadow-normal duration-300 hover:shadow-3d-primary tablet-lg:flex-col phone-xl:items-center" key={service.id}>
                             <img src={service.image} alt={service.title} className="h-36 p-1 duration-300 hover:scale-105"/>
                             <div className="flex flex-col gap-2 pr-2 phone-xl:items-center">
                                 <p className="text-md font-semibold mb-1">{language === "kh" ? service.khmerTitle : service.title}</p>
--- src/components/home/service-card/service-skeleton/index.tsx
+++ src/components/home/service-card/service-skeleton/index.tsx
@@ -1,20 +1,12 @@
 import { useTranslation } from "react-i18next";
 import Divider from "../../../utilities/styles/divider";
-import React, { useEffect } from "react";
-import gsap from "gsap";
-import { useDrawerStore } from "../../../../stores/drawer/drawer.store";
+import React from "react";
 
 export default function ServiceSkeleton({ children } : { children: React.ReactNode } ) {
     const { t } = useTranslation()
-    const isOpen = useDrawerStore((state) => state.isOpen)
-    
-    useEffect(() => {
-        gsap.from("#service-box", { duration: 1, x: 500, opacity: 0, ease: "power1.out"})
-        gsap.to("#service-box", { duration: 1, x: 0, opacity: 1, ease: "power1.out" })
-    }, [])
-    
+        
     return (
-        <div id={`${!isOpen && "service-box"}`} className="w-full flex flex-col items-center gap-5">
+        <div className="w-full flex flex-col items-center gap-5">
             <p className="text-3xl text-center font-medium tablet-md:text-2xl phone-xl:text-xl">{t("pages.home.home-service.label")}</p>
             <Divider/>
             {children}
