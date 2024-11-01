15c937a330d08d187dfd809eed44a42ad3d3d7f2
RithyB <ctrcomputer@Bondeths-Macbook-Pro-2.local>
2024-11-01 10:44:58 +0700

    Fix deploy error

--- src/components/utilities/forms/input/index.tsx
+++ src/components/utilities/forms/input/index.tsx
@@ -1,7 +1,10 @@
+import { useLanguageStore } from "../../../../stores/language/language.store";
 import { InputFieldProps } from "./type"
 import { forwardRef } from "react";
 
-export const InputField = forwardRef<HTMLInputElement, InputFieldProps>((props, ref) => {   
+export const InputField = forwardRef<HTMLInputElement, InputFieldProps>((props, ref) => {
+    const language = useLanguageStore((state) => state.language)
+
     return (
         <div className="w-full flex items-center p-4 rounded-xl bg-gray-100 dark:bg-darklight dark:text-dark">
             {props.preffixIcon && (
@@ -12,7 +15,7 @@ export const InputField = forwardRef<HTMLInputElement, InputFieldProps>((props, 
                 type={props.type} 
                 id={props.id}
                 name={props.name} 
-                value={props.value}
+                value={props.value === null ? language === "kh" ? "មិនមាន" : "None" : props.value}
                 placeholder={props.placeholder}
                 onChange={props.onChange}
                 onFocus={props.onFocus}
