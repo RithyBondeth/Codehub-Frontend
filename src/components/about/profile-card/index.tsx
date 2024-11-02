5490f6f4e2fe157303160061f0cb3f7531779927
RithyB <ctrcomputer@Bondeths-Macbook-Pro-2.local>
2024-11-02 10:31:52 +0700

    fix founder image

--- src/components/about/profile-card/index.tsx
+++ src/components/about/profile-card/index.tsx
@@ -18,7 +18,7 @@ export default function ProfileCard() {
         <div className="my-10 flex flex-col justify-start gap-10">
             <div className="flex justify-between items-stretch gap-5 tablet-md:flex-col tablet-md:items-center">
                 <div id="profile-box" className="w-[35%] flex flex-col items-center tablet-md:w-1/2">
-                    <img src="/src/assets/about/bondeth.jpg" alt="Codehub Founder" className="w-full"/>
+                    <img src="/public/bondeth.jpg" alt="Codehub Founder" className="w-full"/>
                     <p className="mt-3 text-sm text-gray-500 tablet-md:text-center tablet-md:leading-8">{t("pages.about.profile.profile-imagelabel")}</p>
                 </div>
                 <div id="profile-box" className="w-[65%] leading-10 px-5 text-md text-gray-500 tablet-md:w-full">
